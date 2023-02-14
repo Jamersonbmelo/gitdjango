9cb3fba1239050a8aaf3f50a185fea4e88848d23
Jamerson Melo <jamersonjosealvaro@gmail.com>
2023-02-14 16:04:15 -0300

    terceiro commit

--- emprestimos/urls.py
+++ emprestimos/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
+from .views import listar_livros, detalhar_livro
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('emprestimos/', include('emprestimos.urls')),
-    path('admin/', admin.site.urls),
+    path('listar_livros/', listar_livros, name='listar_livros'),
+    path('detalhar_livro/<int:pk>', detalhar_livro, name='detalhar_livro'),
 ]
--- emprestimos/views.py
+++ emprestimos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Livro
 
 # Create your views here.
+
+def listar_livros(request):
+    livros = Livro.objects.all()
+    return render(request, 'listar_livros.html', {'livros':livros})
+
+def detalhar_livro(request, pk):
+    livro = Livro.objects.get(pk=pk)
+    return render(request, 'detalhar_livro.html', {'livro':livro})
